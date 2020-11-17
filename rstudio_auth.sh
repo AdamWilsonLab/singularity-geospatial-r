0979e941deede235030e12f3b87da6e135119289
Adam M. Wilson <adammichaelwilson@gmail.com>
2020-11-16 21:24:32 -0500

    Update rstudio_auth.sh

--- rstudio_auth.sh
+++ rstudio_auth.sh
@@ -3,6 +3,7 @@
 # from https://github.com/nickjer/singularity-rstudio/blob/master/rstudio_auth.sh
 
 # Confirm username is supplied
+# edited because https://github.com/grst/rstudio-server-conda/issues/3
 if [[ $# -lt 1 ]]; then
   echo "Usage: auth USERNAME"
   exit 1
