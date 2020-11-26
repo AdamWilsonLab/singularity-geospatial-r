4ae21dbf1a9aed4bc592a2feeb0f1e0ba2b5b077
Adam M. Wilson <adammichaelwilson@gmail.com>
2020-11-25 22:00:06 -0500

    update environment and try install2.r

--- run_script.sh
+++ run_script.sh
@@ -1,8 +1,28 @@
+#! /usr/bin/env bash
+
+# Details for CCR
+
+
 # pull image
-cd /panasas/scratch/grp-adamw/;
-singularity pull -F shub://AdamWilsonLab/singularity-geospatial-r
+# cd /panasas/scratch/grp-adamw/;
+#singularity pull -F shub://AdamWilsonLab/singularity-geospatial-r
 
 
+SINGULARITY_LOCALCACHEDIR=/panasas/scratch/grp-adamw/singularity/
+SINGULARITY_CACHEDIR=/panasas/scratch/grp-adamw/singularity/
+SINGULARITY_TMPDIR=/panasas/scratch/grp-adamw/singularity/
+export SINGULARITY_LOCALCACHEDIR
+export SINGULARITY_CACHEDIR
+export SINGULARITY_TMPDIR
+
+#export SINGULARITY_BINDPATH="$SINGULARITY_TMPDIR:/var/run,/active,/archive"
+#export PATH="/usr/local/bin:/usr/lib/rstudio-server/bin:$PATH"
+#export SINGULARITYENV_PATH="$PATH"
+#export MODULEPATH="/depot/Modules/modulefiles"
+
+
+mkdir -p $SINGULARITY_LOCALCACHEDIR
+newgrp grp-adamw
 mkdir mytmp myrun
 
 
@@ -14,19 +34,18 @@ echo $PASSWORD
 
 #    -B $(pwd):/home/rstudio -B $(pwd):$HOME \
 
-
-singularity exec \
-    --bind /projects/academic/adamw/ \
-    -B mytmp:/tmp --bind myrun:/run \
-    singularity-geospatial-r_latest.sif \
-    rserver --www-port ${PORT} --auth-none=0 --auth-pam-helper-path=pam-helper
+#singularity exec \
+#    --bind /projects/academic/adamw/ \
+#    -B mytmp:/tmp --bind myrun:/run \
+#    singularity-geospatial-r_latest.sif \
+#    rserver --www-port ${PORT} --auth-none=0 --auth-pam-helper-path=pam-helper
 
 
 # use an instance
 singularity instance start --bind /projects/academic/adamw/ \
 -B mytmp:/tmp --bind myrun:/run \
 singularity-geospatial-r_latest.sif \
-rserver
+rserver  --www-port ${PORT} --auth-none=0 --auth-pam-helper-path=pam-helper
 
 
-singularity exec instance://rserver rserver --www-port ${PORT} --auth-none=0 --auth-pam-helper-path=pam-helper
+# singularity exec instance://rserver rserver --www-port ${PORT} --auth-none=0 --auth-pam-helper-path=pam-helper
