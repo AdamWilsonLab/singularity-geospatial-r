04e3e99143e37b8e01d9ff66aa160843fe50583f
Adam M. Wilson <adammichaelwilson@gmail.com>
2020-12-02 22:49:47 -0500

    Update singularity_start.sh

--- singularity_start.sh
+++ singularity_start.sh
@@ -1,28 +1,34 @@
 #! /usr/bin/env bash
 
-export SINGULARITY_LOCALCACHEDIR=/panasas/scratch/grp-adamw/singularity/$USER
-export SINGULARITY_CACHEDIR=$SINGULARITY_LOCALCACHEDIR
-export SINGULARITY_TMPDIR=$SINGULARITY_LOCALCACHEDIR
+SINGULARITY_LOCALCACHEDIR=/panasas/scratch/grp-adamw/singularity/$USER
+SINGULARITY_CACHEDIR=$SINGULARITY_LOCALCACHEDIR
+SINGULARITY_TMPDIR=$SINGULARITY_LOCALCACHEDIR
 
-#export SINGULARITY_LOCALCACHEDIR
-#export SINGULARITY_CACHEDIR
-#export SINGULARITY_TMPDIR
+export SINGULARITY_LOCALCACHEDIR
+export SINGULARITY_CACHEDIR
+export SINGULARITY_TMPDIR
 
 mkdir -p $SINGULARITY_LOCALCACHEDIR
-mkdir -p $SINGULARITY_LOCALCACHEDIR/mytmp
-mkdir -p $SINGULARITY_LOCALCACHEDIR/myrun
+mkdir -p $SINGULARITY_LOCALCACHEDIR/tmp
+mkdir -p $SINGULARITY_LOCALCACHEDIR/run
 
 newgrp grp-adamw
-
 
 #  New method following https://www.rocker-project.org/use/singularity/
 readonly PORT=$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
 export PASSWORD=$(openssl rand -base64 15)
 
 singularity exec --bind /projects/academic/adamw/ \
--B $SINGULARITY_LOCALCACHEDIR/mytmp:/tmp --bind $SINGULARITY_LOCALCACHEDIR/myrun:/run \
+-B $SINGULARITY_LOCALCACHEDIR/tmp:/tmp --bind $SINGULARITY_LOCALCACHEDIR/run:/run \
 /panasas/scratch/grp-adamw/singularity/singularity-geospatial-r_latest.sif \
 rserver  --www-port ${PORT} --auth-none=0 --auth-pam-helper-path=pam-helper &
+
+# use an instance
+#singularity instance start --bind /projects/academic/adamw/ \
+#-B $SINGULARITY_LOCALCACHEDIR/tmp:/tmp --bind $SINGULARITY_LOCALCACHEDIR/run:/run \
+#/panasas/scratch/grp-adamw/singularity/singularity-geospatial-r_latest.sif \
+#rserver  --www-port ${PORT} --auth-none=0 --auth-pam-helper-path=pam-helper
+
 
 
 cat 1>&2 <<END
@@ -40,28 +46,15 @@ cat 1>&2 <<END
     When done using RStudio Server, terminate the job by:
 
     1. Exit the RStudio Session ("power" button in the top right corner of the RStudio window)
-    2. Issue the following command on the login node:
-
-          singularity instance stop rserver
 END
 
-
-#singularity exec \
-#    --bind /projects/academic/adamw/ \
-#   -B mytmp:/tmp --bind myrun:/run \
-#    singularity-geospatial-r_latest.sif \
-#    rserver --www-port ${PORT} --auth-none=0 --auth-pam-helper-path=pam-helper
+#    2. Issue the following command on the login node:
+#
+#          singularity instance stop rserver
 
 
+#singularity exec instance://rserver rserver
 
-# use an instance
-#singularity instance start --bind /projects/academic/adamw/ \
-#-B $SINGULARITY_LOCALCACHEDIR/mytmp:/tmp --bind $SINGULARITY_LOCALCACHEDIR/myrun:/run \
-#/panasas/scratch/grp-adamw/singularity/singularity-geospatial-r_latest.sif \
-#rserver  --www-port ${PORT} --auth-none=0 --auth-pam-helper-path=pam-helper
-
-#echo ssh -N -L 8787:${HOSTNAME}:${PORT} ${USER}@horae.ccr.buffalo.edu
-#echo ssh -N -L 8787:hor:${PORT} ${USER}@hor
 
 #singularity exec --bind /projects/academic/adamw/ \
 #-B $SINGULARITY_LOCALCACHEDIR/mytmp:/tmp --bind $SINGULARITY_LOCALCACHEDIR/myrun:/run \
